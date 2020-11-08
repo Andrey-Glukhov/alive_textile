4e8c342c62b86e10ac8f730536e5b9c9c0db0bf9
Eliza <gluhova.liza@gmail.com>
2020-11-08 13:58:20 +0100

    first commit

---
+++ navigation.php
@@ -0,0 +1,35 @@
+<nav>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6 events_header">EVENTS</div>
+      <div class="col-6 ongoing_header">ONGOING</div>
+  </div>
+</div>
+  <div class="menu-btn">
+    <div class="animated-icon1"><span></span><span></span><span></span></div>
+  </div>
+
+  <div class="menu" style="padding:0;">
+          <div class="nav_content">
+            <ul>
+              <?php
+              wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'container' => false,
+                'menu_class' => 'auto-navigation',
+                'items_wrap' => '<div id="%1$s" class="navigation-bar %2$s">%3$s</div>',
+                'item_spacing' => 'preserve'
+              )
+            );
+            ?>
+          </ul>
+        </div>
+      </div>
+</nav>
+<div class="category-nav">
+<div class="r_d"></div>
+<div class="r_d"></div>
+<div class="r_d"></div>
+<div class="r_d"></div>
+<div class="r_d"></div>
+</div>
---
+++ team.php
@@ -0,0 +1,8 @@
+<?php
+/**
+*Template Name: Team Page
+*/
+get_header(); ?>
+
+
+<?php get_footer(); ?>
