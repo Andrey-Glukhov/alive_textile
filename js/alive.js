9a084584e667436f2cab9385e9b5e0ebab023f9e
Andrey-Glukhov <agi2000@mail.ru>
2020-11-08 18:05:55 +0300

    correct stickers

--- js/alive.js
+++ js/alive.js
@@ -12,10 +12,12 @@ $(document).ready(function () {
   });
   if ($('.timeline').length) {
      controller = new ScrollMagic.Controller();
+     gsap.defaultOverwrite = false;
+     var tweenSet = gsap.fromTo('.menu_sticker', {top:'-150%'}, {top:0, duration: 0.5} );
      var scene = new ScrollMagic.Scene({
         triggerElement: ".timeline",
         triggerHook: 0.1})
-          .setTween(".menu_sticker", 0.5, {top:0})
+          .setTween(tweenSet)
 					.addIndicators() // add indicators (requires plugin)
           .addTo(controller);
           
