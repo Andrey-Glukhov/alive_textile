a37e72e27e14129a55a20cc5b68cd550081a79f2
Eliza <gluhova.liza@gmail.com>
2021-01-05 07:28:53 +0100

    content

--- single-timeline.php
+++ single-timeline.php
@@ -6,60 +6,51 @@ get_header(); ?>
 
 <div class="page_transition"></div>
 <main data-barba="container" data-barba-namespace="single">
-<a href="http://localhost:8888/alive_textile/wordpress" class="home_link"></a>
-<div class="return_arrow"></div>
-<?php if(have_posts() ) : while (have_posts() ) :the_post();
+	<a href="http://localhost:8888/alive_textile/wordpress" class="home_link"></a>
+	<div class="return_arrow"></div>
+	<?php if(have_posts() ) : while (have_posts() ) :the_post();
 	$cat = get_the_category();
 	if (count($cat) <=0 ) {
 		$cat = array('no_cat');
-				}
-		?>
-<section class="container-fluid single_item <?php the_field('event_type');?>_color">
-<div class="single_opener" id="opener_canvas"></div>
-<article class="row justify-content-center single_item_header">
-  <div class="col-md-1"></div>
-  <div class="col-md-5 single_<?php the_field('event_type');?>_type"><h2><?php the_field('event_type');?></h2></div>
-  <div class="col-md-5"></div>
-  <div class="col-md-1"></div>
-</article>
+	}
+	?>
+	<div class="single_opener" id="opener_canvas"></div>
+	<div class="container-fluid single_item <?php the_field('event_type');?>_color">
+		<div class="row justify-content-center single_item_header">
+			<div class="col-1"></div>
+			<div class="col-sm-4 col-10 single_<?php the_field('event_type');?>_type"><h2><?php echo $cat[0]->name;?></h2></div>
+			<div class="col-sm-2 d-none d-sm-flex type_icon_wrapper"><div class="single_type_icon <?php echo $cat[0]->name;?>"></div></div>
+			<div class="col-sm-4 d-none d-sm-block single_line"></div>
+			<div class="col-1"></div>
+		</div>
 
-<article class="row single_row justify-content-center">
-      <div class="col-md-4 col-sm-10 single_<?php the_field('event_type');?>">
-        <div class="single_<?php the_field('event_type');?>_date"><p><?php the_field('event_date_text');?></p></div>
-        <h2><?php the_title();?></h2>
-      </div>
+		<div class="row single_row justify-content-center">
+			<div class="col-md-5 col-sm-10 col-10 single_<?php the_field('event_type');?>">
+				<div class="single_<?php the_field('event_type');?>_date"><p><?php the_field('event_date_text');?></p></div>
+				<h2><?php the_title();?></h2>
+			</div>
 
-      <div class="col-md-1"></div>
+			<div class="col-md-5 col-sm-10d-none d-sm-none d-md-block"></div>
+		</div>
 
-      <div class="col-md-4 col-sm-10"></div>
-</article>
+		<div class="row single_row justify-content-center">
 
-<article class="row single_row justify-content-center">
+			<div class="col-md-5 col-sm-10 col-10 single_<?php the_field('event_type');?>">
+				<div class="single_<?php the_field('event_type');?>_img"><img src="<?php the_field('event_image');?>"/></div>
+			</div>
 
-      <div class="col-md-4 col-sm-10 single_<?php the_field('event_type');?>">
-        <div class="single_<?php the_field('event_type');?>_img"><img src="<?php the_field('timeline_image');?>"/></div>
-      </div>
+			<div class="col-md-5 col-sm-10 col-10 single_short_desc">
+				<p>
+					<?php the_field('second_column');?>
+				</p>
+			</div>
+		</div>
 
-      <div class="col-md-1"></div>
-
-      <div class="col-md-4 col-sm-10 single_short_desc">
-        <p>
-          <?php the_field('second_column');?>
-        </p>
-      </div>
-
-</article>
-
-
-<article class="fluid_content">
-
-  <?php the_content();?>
-
-</article>
-</section>
+		<div class="fluid_content">
+			<?php the_content();?>
+		</div>
+	</div>
 <?php endwhile; ?>
 <?php endif; ?>
-
+</main>
 <?php get_footer(); ?>
-
-
